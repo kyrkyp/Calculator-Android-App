a74b0b9071718188e935fe541c3f0dda6bb2769e
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-05-26 13:55:50 +0300

    Add project files.

---
+++ DemoMobileApp/DemoMobileApp/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DemoMobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private Label label;
+        private int number1;
+        private int number2;
+        private string symbol;
+        private bool isFirstClick = true;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            label = this.FindByName<Label>("text");
+        }
+
+        private void HandleClickOnNumber(object sender, EventArgs e)
+        {
+            string num = (sender as Button).Text;
+            label.Text += " " + num;
+
+            if (isFirstClick)
+            {
+                isFirstClick = false;
+                number1 = int.Parse(num);
+            }
+            else
+            {
+                number2 = int.Parse(num);
+            }
+        }
+
+        private void HandleClickOnSymbol(object sender, EventArgs e)
+        {
+            string sign = (sender as Button).Text;
+            symbol = sign;
+            label.Text += " " + sign;
+        }
+
+        private void HandleClickOnClear(object sender, EventArgs e)
+        {
+            isFirstClick = true;
+            label.Text = "";
+        }
+
+        private void HandleClickOnEquals(object sender, EventArgs e)
+        {
+            int result = 0;
+
+            switch (symbol)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "*":
+                    result = number1 * number2;
+                    break;
+
+                case "%":
+                    result = number1 % number2;
+                    break;
+            }
+
+            label.Text += " = " + result.ToString();
+        }
+    }
+}
